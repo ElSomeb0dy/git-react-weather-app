0de47c89d3203470fe3becc9842f0cde8884b016
ethan.carrouee <ethan.carrouee@epita.fr>
2026-03-09 19:01:34 +0100

    feat(tests): forgotten setup reapplied

--- jest.setup.ts
+++ jest.setup.ts
@@ -14,3 +14,32 @@ jest.mock("expo-secure-store", () => ({
 jest.mock("@react-native-async-storage/async-storage", () =>
     require("@react-native-async-storage/async-storage/jest/async-storage-mock")
 );
+
+jest.mock("react-native-reanimated", () =>
+    require("react-native-reanimated/mock")
+);
+
+jest.mock("react-native-gesture-handler", () => ({
+    GestureHandlerRootView: ({ children }: any) => children,
+    Swipeable: "Swipeable",
+    DrawerLayout: "DrawerLayout",
+    State: {},
+    PanGestureHandler: "PanGestureHandler",
+    BaseButton: "BaseButton",
+    RectButton: "RectButton",
+    BorderlessButton: "BorderlessButton",
+    FlatList: require("react-native").FlatList,
+    ScrollView: require("react-native").ScrollView,
+    Directions: {},
+}));
+
+jest.mock("react-native-gesture-handler/ReanimatedSwipeable", () => {
+    const React = require("react");
+    const { View } = require("react-native");
+    const ReanimatedSwipeable = React.forwardRef(
+        (props: any, _ref: any) =>
+            React.createElement(View, null, props.renderRightActions?.(), props.children)
+    );
+    ReanimatedSwipeable.displayName = "ReanimatedSwipeable";
+    return { __esModule: true, default: ReanimatedSwipeable, SwipeableMethods: {} };
+});
