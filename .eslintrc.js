157d82c46b5517ce5fa372b1248663d8af577a66
ethandesk <ethan>
2026-03-23 18:58:10 +0100

    feat: added ci implementation (+ eslint and prettier)

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  settings: {
+    react: { version: "detect" },
+  },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-explicit-any": "warn",
+  },
+};
